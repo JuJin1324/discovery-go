661b3a370f5c3b8c02265a5bd0c0291c80910d25
JuJin1324 <jujin1324@daum.net>
2020-05-13 12:36:20 +0900

    file io 완료/ set 완료 / graph 보강 필요

--- chapter3/map/map_test.go
+++ chapter3/map/map_test.go
@@ -1,5 +1,63 @@
 package _map
 
-func count(s string, codeCount map[rune]int) {
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"testing"
+)
 
+func count(s string, codeCount map[rune]int) {
+	for _, r := range s {
+		codeCount[r]++
+	}
+}
+
+func TestCount1(t *testing.T) {
+	codeCount := map[rune]int{}
+	count("가나다나", codeCount)
+	if !reflect.DeepEqual(map[rune]int{'가': 1, '나': 2, '다': 1}, codeCount) {
+		t.Error("codeCount mismatch:", codeCount)
+	}
+}
+
+func TestCount2(t *testing.T) {
+	codeCount := map[rune]int{}
+	count("가나다나", codeCount)
+	if len(codeCount) != 3 {
+		t.Error("codeCount:", codeCount)
+		t.Fatal("counet should be 3 but:", len(codeCount))
+	}
+	if codeCount['가'] != 1 || codeCount['나'] != 2 || codeCount['다'] != 1 {
+		t.Error("codeCount mismatch:", codeCount)
+	}
+}
+
+func ExampleCount1() {
+	codeCount := map[rune]int{}
+	count("가나다나", codeCount)
+	for _, key := range []rune{'가', '나', '다'} {
+		fmt.Println(string(key), codeCount[key])
+	}
+	// Output:
+	// 가 1
+	// 나 2
+	// 다 1
+}
+
+func ExampleCount2() {
+	codeCount := map[rune]int{}
+	count("가나다나", codeCount)
+	var keys sort.IntSlice
+	for key := range codeCount {
+		keys = append(keys, int(key)) // key : rune to int
+	}
+	sort.Sort(keys)
+	for _, key := range keys {
+		fmt.Println(string(key), codeCount[rune(key)])
+	}
+	// Output:
+	// 가 1
+	// 나 2
+	// 다 1
 }
